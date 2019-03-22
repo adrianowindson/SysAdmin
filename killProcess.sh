66bb958c98bf27776a1b97f5f3c8dbd1876dc4ba
Adriano Lima <adrianowindsonbjj@gmail.com>
2019-03-22 00:21:00 -0300

    ;*

---
+++ killProcess.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+##################################################################
+# SCRIPT PARA AUTOMATIZAÇÃO DE FINALIZAÇÃO DE PROCESSO PELO KILL #
+##################################################################
+
+
+echo 'Digite o nome do processo a ser finalizado: '
+read processo
+
+killProcess=$(ps aux| grep $processo| head -n1 | cut -d " " -f4)
+
+echo 'Esse é o processo que quer finalizar:' $killProcess
+echo '1-P/Sim Ou 2-P/Não'
+read resposta
+
+if [ $resposta -eq 1 ]
+then
+	kill $killProcess
+	echo 'Finalizado'
+else
+ 	echo 'Saindo do Bash' | sleep 10 | exit
+fi
