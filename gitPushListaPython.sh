2a580348c623cc91b30f073c2677d7a2c544e9c0
Adriano Lima <adrianowindsonbjj@gmail.com>
2019-05-14 22:24:05 -0300

    0-0

--- gitPushListaPython.sh
+++ gitPushListaPython.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+##########################################################
+# VERIFICA SE HÁ ARQUIVO A SER ADICIONADO NO REPOSITÓRIO #
+##########################################################
+
 cd Documentos/ListaDeExercicios/EstruturaDeRepeticao ; ls -l ; git status
